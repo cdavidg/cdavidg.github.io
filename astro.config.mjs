6cb34427283edfab9e48c46b6ffcb249aed0606d
dav_25 <67080571+cdavidg@users.noreply.github.com>
2025-10-11 15:38:57 -0400

    Clean repository for React app migration - Remove Astro project files

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://cdavidg.github.io',
-  base: '/',
-  output: 'static',
-  trailingSlash: 'ignore'
-});
